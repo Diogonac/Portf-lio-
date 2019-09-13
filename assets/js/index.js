b06cc207a70a680541d7520602b458884b83c9d4
Joao-Pedro-Chacon-Ruiz <joaopedroruiz2001@gmail.com>
2019-09-13 15:01:56 -0300

    inicio da integração cpm firebase

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let db = coDesConnect('https://portif-27880.firebaseio.com/')
+
+  db.download('/portfolio', function(data) {
+
+    /*
+      Sim, abaixo estamos usando "in" em vez de "of", pois
+      "of" você usa para iterar em listas. Para iterar nas
+      chaves de um dicionário, usamos "in", como em Python.
+      Sim, é confuso. Sinto muito.
+    */
+    for(let key in data) {
+
+      /*
+        O resultado deste loop pode ser visto no console.
+      */
+      console.log(data[key])
+    }
+  })
+})
